8f2384338a7bae2e72a136a653833cb48892c75b
Adam <aszum@poczta.fm>
2021-03-12 07:47:51 +0100

    files added

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+const name = "Marcin";
+
+if (name === "Marcin") {
+    console.log("Marciny są fajne");
+} else if (name === "Ania") {
+    console.log("Anie są fajne");
+} else if (name === "Radosław") {
+    console.log("Radki są fajne");
+} else {
+    console.log("Nie wiem kto jest fajny");
+}

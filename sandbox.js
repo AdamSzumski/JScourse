9f7199292a910409a0b47a491b8b07c0b9e177a1
Adam <aszum@poczta.fm>
2021-03-12 11:06:08 +0100

    changes

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+1. // Napisz funkcję, która dodaje 2 liczby i zwraca wynik (z użyciem return). Nazwij funkcję sum.
+
+
+function sum (a, b){
+        sum = 5 + 5;
+    return sum;
+}
+
+// - przypisz wywołanie sum(5, 5) funkcji do zmiennej ten
+// - przypisz wywołanie sum(2, 2) funkcji do zmiennej four
+// - wykorzystaj funkcję sum, aby dodać wartości zmiennych ten i four. Wynik przypisz do zmiennej i wydrukuj wartość tej zmiennej na konsoli.
+
+
+
+// const sum = (2, 2);
+
+const guests =['jan', 'lolo', 'gg']
+const firstGuest = guests[0];
+console.log(secondGuest);
